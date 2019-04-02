47a0bd185df000748acdfad262030124e2a09414
onlyserenity <19535323+onlyserenity@users.noreply.github.com>
2019-04-02 18:00:21 -0400

    added manaul solution to the anagram problem

--- Strings/Anagram.py
+++ Strings/Anagram.py
@@ -11,6 +11,29 @@ def anagram_check_pythonic(s1, s2):
     return sorted(s1) == sorted(s2)
 
 
+# This function perform more manual way of checking 
+# NOTE: assuming both strings do not include any spaces
+def anagram_check_without_spaces(s1, s2):
+    if (len(s1) != len(s2)):
+        return False
+
+    count = {}
+    for index in s1:
+        if index in count:
+            count[index] += 1
+        else:
+            count[index] = 1
+
+    for index in s2:
+        if index in count:
+            count[index] -= 1
+        else:
+            count[index] = 1
+
+    for x in count:
+        if count[x] != 0:
+            return False
+    return True
 # Below should return true
 print(anagram_check_pythonic("god", " d o g"))
 
