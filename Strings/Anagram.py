6115aa0e8936738e6a9e08f7c520fe20f8d4897d
onlyserenity <19535323+onlyserenity@users.noreply.github.com>
2019-04-02 18:23:24 -0400

    add with spaces solution to anagram

--- Strings/Anagram.py
+++ Strings/Anagram.py
@@ -34,6 +34,36 @@ def anagram_check_without_spaces(s1, s2):
         if count[x] != 0:
             return False
     return True
+
+
+# This function perform more manual way of checking 
+# NOTE: assuming both strings may include any spaces
+def anagram_check_without_spaces(s1, s2):
+    # Pythonic way to replace empty spaces
+    s1 = s1.replace(' ', '').lower()
+    s2 = s2.replace(' ', '').lower()
+    if (len(s1) != len(s2)):
+        return False
+
+    count = {}
+    for index in s1:
+        if index in count:
+            count[index] += 1
+        else:
+            count[index] = 1
+
+    for index in s2:
+        if index in count:
+            count[index] -= 1
+        else:
+            count[index] = 1
+
+    for x in count:
+        if count[x] != 0:
+            return False
+    return True
+
+
 # Below should return true
 print(anagram_check_pythonic("god", " d o g"))
 
