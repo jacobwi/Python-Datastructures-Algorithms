1f7a5e054aa3bcd8a19611927a5d23b67814d3f3
onlyserenity <19535323+onlyserenity@users.noreply.github.com>
2019-04-03 20:30:49 -0400

    added reverse integer problem

--- ReverseProblems/ReverseInteger.py
+++ ReverseProblems/ReverseInteger.py
@@ -4,4 +4,14 @@
 def reverse(x):
     print(int(str(x)[::-1]))
 
+def reverseAny(x):
+    answer = 0
+    if x >= 0:
+        answer = int(str(x)[::-1]) 
+    else:
+        answer = -int(str(-x)[::-1])
+
+    return answer
+
 reverse(123)
+print(reverseAny(-432))
