468672b12e37d56799a30d8ed4848043035560e8
Jacob William <jacobwilliam@Jacobs-MacBook-Pro.local>
2019-09-09 18:21:03 -0400

    added array rotate solution

---
+++ ArrayListProblems/RotateArray.py
@@ -0,0 +1,17 @@
+
+class Solution(object):
+
+    def rotate(self, nums, k):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        print("elements we're rotating to the left\n",nums[-k:])
+        print("elements we're rotating to the right\n",nums[:-k])
+        nums[k:], nums[:k] = nums[:-k], nums[-k:]
+        return nums
+
+nums = [1,2,3,4,5,6,7]
+k = 3
+ex = Solution()
+print(ex.rotate(nums, k))
